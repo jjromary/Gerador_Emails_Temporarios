c0dc3de1038763237cc4c5ac21894eaf6d333e86
José Romary Brandão <joseromarybrandao@gmail.com>
2023-02-14 16:13:33 -0300

    add: routes and implement themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Router } from "./Routes";
+import { GlobalStyles } from "./Styles/Global";
+import { defaultTheme } from "./Styles/Themes/default";
+
 export default function App() {
   return (
-    <div>
-      hello romary
-    </div >
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
 
